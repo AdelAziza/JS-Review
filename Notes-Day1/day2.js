bbd834f803231ef1188928b1c512c50f6ecdf028
AdelAziza <adelazizauseni@gmail.com>
2021-10-07 10:26:38 -0700

    10/07/2021

--- Notes-Day1/day2.js
+++ Notes-Day1/day2.js
@@ -10,8 +10,8 @@ function myChange(){
 
 //Change images 
 let myImage='/images/adel2.jpeg';
-
-function changeImage1(){
+//expression function
+const valuF = function(){
     document.getElementById('img1').src=myImage;
 }
 
@@ -46,3 +46,84 @@ var age=26, yearWeeding=2022, dreamVac='Paris And Dubai';
 function whatMyDreamDay(){
     document.write(`Start with ${userName} my age is ${age}  in ${yearWeeding} I will be married  and I will go to ${dreamVac} `)
 }
+
+function calCulateMyAge(){
+    // let mynameWord='I am '
+    let myfirst=26;
+    let mysecond='26';
+    if(myfirst===mysecond || myfirst==myfirst){
+        
+        document.write('You are not 26 years old');
+       
+    }
+    else{
+       
+        document.write('You are 26 years old');
+    }
+
+    // let age=5;
+// {/* <h3>The ** Operator</h3> */}  
+}
+
+function objectPratice(){
+    const myInfo={
+        nameU: 'Adel Aziza',
+        age:26,
+        hobbie:'Nigerian Movies'
+
+    }
+    document.write('My name is '+ ' '+ myInfo.nameU + ' '+ 'I am ' + ' ' +
+    myInfo.age + ' ' + ' I love'+ ' ' + myInfo.hobbie);
+}
+
+/*          -------------JavaScript Functions---------------
+A JavaScript function is a block of code designed to perform a particular task.
+
+A JavaScript function is executed when "something" invokes it (calls it).
+
+
+Ways of defining a function
+1..... function nameOfTheFunction(){
+
+}
+
+2.....Expression
+const constvalue= function(){
+
+}
+
+3......
+*/
+
+function calF(){
+const myAcademiJourney={
+    associate:2019,
+    bachelor:2021,
+    school:'Bellevue College',
+    city:function(){
+        state:'WA'
+        return 'I got my associate in '+ ' ' + this.associate;
+    }
+}
+}
+
+//Ways of defining a function
+//1.....Normal 
+ function nameOfTheFunction(){
+
+}
+
+//2.....Expression
+const constvalue= function(){
+
+}
+
+/*3...... The Function() Constructor
+As you have seen in the previous examples, JavaScript functions are defined with the function keyword.
+
+Functions can also be defined with a built-in JavaScript function constructor called Function().*/
+const myFunction = new Function("a", "b", "return a * b");
+
+let x = myFunction(4, 3);
+/*output:JavaScript has an built-in function constructor.
+12*/
